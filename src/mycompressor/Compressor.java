c6741729f9b990296feff0294b130646fc40c1ef
ahmed.ibrahim <ahmed.ibrahim.mohamed1992@gmail.com>
2016-08-03 16:25:15 +0200

    update project

---
+++ src/mycompressor/Compressor.java
@@ -0,0 +1,26 @@
+package mycompressor;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rwsv2308
+ */
+public class Compressor {
+    public boolean compress(String srcFile, String distFile){
+        return true;
+    }
+    public boolean compress(String srcFile, String distFile, double compressionRatio){
+        return true;
+    }
+    public boolean compress(String srcFile, String distFile, int iterations){
+        return true;
+    }
+    public boolean compress(String srcFile, String distFile, float targetSize){
+        return true;
+    }
+}
---
+++ src/mycompressor/Decompressor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycompressor;
+
+/**
+ *
+ * @author rwsv2308
+ */
+public class Decompressor {
+    public boolean decompress(String srcFile, String distFile){
+        return true;
+    }
+}
