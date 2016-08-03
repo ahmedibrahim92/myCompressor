5df1391497f3ae4229a421664f935ba14dde5313
ahmed.ibrahim <ahmed.ibrahim.mohamed1992@gmail.com>
2016-08-03 16:43:44 +0200

    add comments

--- src/mycompressor/Compressor.java
+++ src/mycompressor/Compressor.java
@@ -14,12 +14,15 @@ public class Compressor {
     public boolean compress(String srcFile, String distFile){
         return true;
     }
+    // 0.001
     public boolean compress(String srcFile, String distFile, double compressionRatio){
         return true;
     }
+    // 20
     public boolean compress(String srcFile, String distFile, int iterations){
         return true;
     }
+    // 0.003 mb
     public boolean compress(String srcFile, String distFile, float targetSize){
         return true;
     }
